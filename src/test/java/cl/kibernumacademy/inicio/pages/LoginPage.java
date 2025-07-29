97d4b34a50e33884a312df51276b4faf191f4a5b
Miguel Ramos <return.infox@gmail.com>
2025-07-28 20:23:26 -0400

    object pages

---
+++ src/test/java/cl/kibernumacademy/inicio/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package cl.kibernumacademy.inicio.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+  private WebDriver driver;
+  // Localizadores
+  private By usernameField = By.id("username");
+  private By passwordField = By.id("password");
+  private By loginButton = By.cssSelector("button[type='submit']");
+
+  public LoginPage(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public void enterUsername(String username) {
+    driver.findElement(usernameField).sendKeys(username);
+  }
+
+  public void enterPassword(String password) {
+    driver.findElement(passwordField).sendKeys(password);
+  }
+
+  public void clickLoginButton() {
+    driver.findElement(loginButton).click();
+  }
+
+  public String getWelcomeMessage() {
+    return driver.findElement(By.id("welcome-message")).getText();
+  }
+  
+}
